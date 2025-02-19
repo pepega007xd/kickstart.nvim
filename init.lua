4f10d59ac4ffdd2b6366b12acd2fd696eb6bfc92
pepega007xd <brablect@gmail.com>
2025-02-19 22:39:53 +0100

    scroll by single line, use fish as shell, use colorscheme with warm colors

--- init.lua
+++ init.lua
@@ -153,6 +153,7 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 20
+vim.opt.mousescroll = 'ver:1'
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -258,6 +259,7 @@ vim.opt.rtp:prepend(lazypath)
 --    :Lazy update
 --
 -- NOTE: Here is where you install your plugins.
+---@diagnostic disable-next-line: missing-fields
 require('lazy').setup {
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
@@ -816,21 +818,11 @@ require('lazy').setup {
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+  { -- colorscheme
+    'rebelot/kanagawa.nvim',
+    priority = 1000,
     init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      vim.cmd.colorscheme 'kanagawa'
     end,
   },
 
@@ -1006,6 +998,7 @@ local function jump_to_last_line()
     end
   end
 end
+
 vim.api.nvim_create_autocmd({ 'BufWinEnter', 'FileType' }, {
   group = vim.api.nvim_create_augroup('nvim-lastplace', {}),
   callback = jump_to_last_line,
@@ -1015,6 +1008,9 @@ vim.filetype.add { extension = { re = 'reason' } }
 
 -- the ocamllsp installed through Mason was too old
 require('lspconfig').ocamllsp.setup {}
+
+-- use fish shell
+vim.o.shell = '/usr/bin/fish'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
